b41bf0339fcd1f0232299b25c2e8840d6bf9f22f
Leonam Candido <leonamcandido80@gmail.com>
2025-03-26 10:20:52 -0300

    meu terceiro commit

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("AUla 05");
+        System.out.println("Fatec Ferraz");
     }
 }
